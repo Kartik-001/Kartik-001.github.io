04e9fe3d1aafa0130e094dd82301f792ce270366
k1rtik <115542829+Kartik-001@users.noreply.github.com>
2025-08-07 00:53:32 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  // URL of your published site
+  site: "https://kartik-001.github.io",
+
+  // omit `base` when deploying to a root-level repo (i.e. <username>.github.io)
+  // base: "/your-repo-name",
+
   vite: {
     plugins: [tailwindcss()],
   },
